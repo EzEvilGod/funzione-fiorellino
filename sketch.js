eb424a2b250b852e946e80996175f0e15843c8bf
EzEvilGod <marco.ferri2005@gmail.com>
2022-11-11 22:32:22 +0100

    funzione pura, creazione fiorellino

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
+let w = 600
+let h = 300
+let d = 80
+let x = x1-x1
+let x1 = w/2
+let y1 = h/2
+let delta = 1
+let f = w
+let DirectionFlower = x - x1
+
 function setup()
 {
-    
+    createCanvas(w,h)
 }
 function draw()
 {
-
+    background("pink")
 }
