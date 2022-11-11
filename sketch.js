668bcf1860d2d995fcec39e6cfedb33c4dbb2cff
EzEvilGod <marco.ferri2005@gmail.com>
2022-11-11 22:52:15 +0100

    controllo funzione e rettificazione

--- sketch.js
+++ sketch.js
@@ -11,40 +11,14 @@ let x = x1-x1
 let f = w
 let directionFiore= x - x1
 
-function setup () {
+let fx
+
+function setup () 
+{
 	createCanvas(w, h)
 }
 
-function draw() {
-	
-	directionFiore = directionFiore + alpha
-	x1 = x1 + alpha
-	x = x + alpha
-	f = f + alpha
-
-	background("pink")
-	drawFiorellino(directionFiore, y, d) 
-
-	drawFiorellino(x1, y, d)					 
-	drawFiorellino(x, y, d)				 
-	drawFiorellino(f, y, d) 	 
-
-	if (x1 == 600) {
-		x = 0
-		x1 = 300
-
-		directionFiore = x - x1
-		f = w
-		let iterazioneFlowerOrigine = directionFiore - x1
-
-		drawFiorellino(iterazioneFlowerOrigine, y, d)		
-	}
-
-
-
-}
-
-function drawFiorellino(x, y, d) {
+function fiore(x, y, d) {
 	let r = d/2
 
 	stroke("rgb(0,0,0)")
@@ -58,3 +32,36 @@ function drawFiorellino(x, y, d) {
 	fill("rgb(255,51,0)")
 	circle(x, y, d)
 }
+
+
+
+
+function draw() {
+	
+	directionFiore = directionFiore + alpha
+	x1 = x1 + alpha
+	x = x + alpha
+	f = f + alpha
+
+	background("pink")
+	fiore(directionFiore, y, d) 
+
+	fiore(x1, y, d)					 
+	fiore(x, y, d)				 
+	fiore(f, y, d) 	 
+
+	if (x1 == 600) {
+		x = 0
+		x1 = 300
+
+		directionFiore = x - x1
+		f = w
+		fx = directionFiore - x1
+
+		fiore(iterazioneFlowerOrigine, y, d)		
+	}
+
+
+
+}
+
